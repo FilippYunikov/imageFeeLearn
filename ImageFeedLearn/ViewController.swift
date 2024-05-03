a702a71f07bce64cec88db5a1776f3d9a6a834af
@rostovHomer <filipprnd161@gmail.com>
2024-05-03 21:01:56 +0300

    Initial Commit

---
+++ ImageFeedLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeedLearn
+//
+//  Created by Filipp on 03.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
