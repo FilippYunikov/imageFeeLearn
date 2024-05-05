d08a9a5be16bcff3a16b19e4a02ef15a06eacf34
FilippYunikov <filipprnd161@gmail.com>
2024-05-05 16:00:18 +0300

    8 спринт

---
+++ ImageFeedLearn/ImagesListViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  ImageFeedLearn
+//
+//  Created by Filipp on 03.05.2024.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    @IBOutlet private var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+
+        cell.cellImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        cell.dateLabel.layer.shadowColor = UIColor.black.cgColor
+        
+        cell.dateLabel.layer.masksToBounds = false
+        cell.dateLabel.layer.shadowColor = UIColor(red: 0.1, green: 0.11, blue: 0.13, alpha: 1.0).cgColor
+        cell.dateLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
+        cell.dateLabel.layer.shadowRadius = 5.0
+        cell.dateLabel.layer.shadowOpacity = 1.0
+
+ 
+
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "heart_no_active") : UIImage(named: "heart_active")
+        cell.likeButton.setImage(likeImage, for: .normal)    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+
+        configCell(for: imageListCell, with: indexPath)
+
+        return imageListCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
